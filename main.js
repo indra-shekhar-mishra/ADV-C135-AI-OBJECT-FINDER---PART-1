d16994bbcbe405beb01f192792e7d12e0316b62e
indra-shekhar-mishra <138847325+indra-shekhar-mishra@users.noreply.github.com>
2024-06-13 22:55:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function setup() {
-    canvas = createCanvas(300,300);
-    canvas.center();
+
+Status = "";
+input_text = "";
+
+function setup(){
+    canvas = createCanvas(300,290);
+    canvas.position(480,250);
     video = createCapture(VIDEO);
-    video.size(300,300);
+    video.size(300,290);
     video.hide();
+}
+function start(){
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    input_text = document.getElementById("input_id").value;
+}
+function modelLoaded(){
+    console.log("Model_Loaded");
+    Status = true;
+}
+function draw(){
+    image(video,0,0,300,290);
 }
