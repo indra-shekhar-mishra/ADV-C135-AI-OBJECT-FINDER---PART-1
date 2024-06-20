ee0055b483ffe122be52a08959ec36f9d92a9dc9
indra-shekhar-mishra <138847325+indra-shekhar-mishra@users.noreply.github.com>
2024-06-20 17:32:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,119 @@
-
-Status = "";
-input_text = "";
-
-function setup(){
-    canvas = createCanvas(300,290);
-    canvas.position(480,250);
+objects = [];
+stat = '';
+input = '';
+abc = 'round 1';
+detectedObjects = [];
+uniqueLabels = '';
+function setup()
+{
+    canvas = createCanvas(380,380);
+    canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,290);
     video.hide();
 }
-function start(){
-    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Objects";
-    input_text = document.getElementById("input_id").value;
+function draw()
+{
+    
+    image(video,0,0,380,380);
+    console.log("IMPORTANT: "+abc);
+
+        if(stat != "")
+    {
+        od.detect(video,gotResults);
+        for (var i=0;i < objects.length;i++) 
+        {
+            document.getElementById("statl").innerHTML = "Status : Objects detected";
+            document.getElementById("len").innerHTML = "Number of objects detected : "+objects.length;
+            document.getElementById('objs').innerHTML="Make sure to reload after your objects are detected to try again"
+
+            fill('red')
+            stroke('red')
+            percent = floor(objects[i].confidence*100)
+            text(objects[i].label+" "+percent+" %",objects[i].x,objects[i].y);
+            noFill();
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects[i].label == inpu)
+            {
+                video.stop();
+                noLoop(); 
+                document.getElementById("statl").innerHTML = "Status : "+inpu+" found";
+                
+                var synth = window.speechSynthesis;
+                speak_data1 = inpu+" found";
+                speak_data3 = "I repeat "+input+"found"
+                var ut = new SpeechSynthesisUtterance(speak_data1+speak_data1+speak_data3);
+                synth.speak(ut); 
+                
+            }
+            else if(abc == 'round 1'){
+                document.getElementById("statl").innerHTML = "Status : "+input+" not found";
+                var synth = window.speechSynthesis;
+                speak_data2 = input+" not found";
+                var ut = new SpeechSynthesisUtterance(speak_data2);
+                synth.speak(ut); 
+                abc = 'round 2'
+                console.log("IMPORTANT: "+abc);
+                
+                
+            }
+            else if(abc == 'round 2')
+            {
+                document.getElementById("statl").innerHTML = "Status : "+input+" not found";
+                var synth = window.speechSynthesis;
+                speak_data2 = "I repeat "+input+" not found";
+                var ut = new SpeechSynthesisUtterance(speak_data2);
+                synth.speak(ut); 
+                abc = 'round 3'
+                console.log("IMPORTANT: "+abc);
+            }
+            else if(abc == 'round 3')
+            {
+                document.getElementById("statl").innerHTML = "Status : "+input+" not found";
+                var synth = window.speechSynthesis;
+                speak_data2 = "For the final time "+input+" not found";
+                var ut = new SpeechSynthesisUtterance(speak_data2);
+                synth.speak(ut); 
+                abc = 'round 4'
+                console.log("IMPORTANT: "+abc);
+            }
+            else if(abc == 'round 3')
+            {
+                document.getElementById("statl").innerHTML = "Status : "+input+" not found";
+                console.log("IMPORTANT: "+"Final round");
+            }
+        }
+        
+    }
+      }
+    
+    
+    
+
+function preload()
+{
+
 }
-function modelLoaded(){
-    console.log("Model_Loaded");
-    Status = true;
+function star()
+{
+    od = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("statl").innerHTML = "Status : Objects are being detected";
+    inpu = document.getElementById("input").value;
 }
-function draw(){
-    image(video,0,0,300,290);
+function modelLoaded()
+{
+    console.log("model has been loaded")
+    stat = true;
+}
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results)
+        objects = results;
+
+    }
 }
